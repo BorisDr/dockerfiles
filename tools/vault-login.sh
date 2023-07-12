0bb25bac3cd486146bbce3e15b69a419733928f1
Boris Efron <borisdr@gmail.com>
2023-07-12 08:21:48 +0200

    vault login

---
+++ tools/vault-login.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Specify Vault role"
+read ROLE
+
+echo "Specify Vault URL
+
+vault-nonprd.devops.pluto.tv
+vault-preprod.devops.pluto.tv
+vault-prod.devops.pluto.tv"
+read DOMAIN
+
+echo "Specify Vault auth backend"
+read AUTH
+
+KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+
+curl --silent --request POST --data '{"jwt": "'"${KUBE_TOKEN}"'", "role": "'"${ROLE}"'"}' https://${DOMAIN}/v1/auth/${AUTH}/login | jq
+echo 'export VAULT_ADDR="https://'${DOMAIN}'"'
